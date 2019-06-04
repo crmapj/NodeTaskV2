471bea536e2c79cae5b620bde19cf82962f19c92
Chris <crmburrett@gmail.com>
2019-06-04 23:32:32 +0100

    Start Rest API

---
+++ src/db/mongoose.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
+    useNewUrlParser: true,
+    useCreateIndex: true
+})
+
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String,
+        trim: true,
+        required: true
+
+    },
+    completed: {
+        type: Boolean,
+        default: false,
+        required: false
+    }
+})
+
